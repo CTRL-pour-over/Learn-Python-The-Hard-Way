f48a41a9071a3c8b1443a71bb1534f9475462c8d
Thomas <tventrone95@gmail.com>
2019-01-24 00:40:27 -0500

    Added ex28, ex29 scripts to the repository

---
+++ ex29.py
@@ -0,0 +1,34 @@
+# The if statements, used in conjunction with the > , < operators will print
+# the following text if True.
+# In other words, ( if x is True: print "a short story" )
+# Indentation is needed for syntax purpouses. if you do not indent you will get
+# "IndentationError: expected an indented block".
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats and 1 == 1: # Prints first
+    print "Too many cats! The world is doomed!"
+
+if people > cats:
+    print "Not many cats! The world is saved!"
+
+if people < dogs:
+    print "The world is drooled on!"
+
+if people > dogs and "test" != "testing": # Prints second
+    print "The world is dry!"
+
+
+dogs += 5 # "increment by" operator. This is the same as 15 + 5 = 20
+
+if people >= dogs: # Prints third
+    print "People are greater than or equal to dogs."
+
+if people <= dogs: # Prints fourth
+    print "People are less than or equal to dogs."
+
+
+if (not people != dogs): # Prints fifth
+    print "People are dogs."
